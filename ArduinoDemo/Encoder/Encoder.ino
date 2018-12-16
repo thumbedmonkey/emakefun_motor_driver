8c72314e17e576df844a232a563852b6b27ed6ce
ken <ken@keywish-robot.com>
2018-12-16 23:05:57 +0800

    Update Motot Shied Board V3.0
    Update Motot Shied Board V3.0

--- ArduinoDemo/Encoder/Encoder.ino
+++ ArduinoDemo/Encoder/Encoder.ino
@@ -1,14 +1,14 @@
 #include "PinChangeInt.h"
-#define AIN1 3
-#define AIN2 11
-#define BIN1 4
-#define BIN2 2
-#define PWMA 5
-#define PWMB 6
-#define STBY 7
 
-#define ENCODER_LEFT_A 13
-#define ENCODER_RIGHT_A 10
+#define EM_MOTOR_SHIELD_BOARD_VERSION 3
+
+#if (EM_MOTOR_SHIELD_BOARD_VERSION > 2)
+    #define ENCODER_LEFT_A 2
+    #define ENCODER_RIGHT_A 7
+#else
+    #define ENCODER_LEFT_A 13
+    #define ENCODER_RIGHT_A 10
+#endif
 
 int left_pluse = 0, right_pluse = 0;
 void Code_left(void)
@@ -27,16 +27,7 @@ void Code_right(void)
 void setup()
 {
     Serial.begin(9600);
-    pinMode(STBY, OUTPUT);
-    pinMode(AIN1, OUTPUT);
-    pinMode(AIN2, OUTPUT);
-	pinMode(BIN1, OUTPUT);
-    pinMode(BIN2, OUTPUT);
-	pinMode(PWMA, OUTPUT);
-    pinMode(PWMB, OUTPUT);
-    digitalWrite(STBY,HIGH);
     pinMode(ENCODER_LEFT_A, INPUT);
-    
     pinMode(ENCODER_RIGHT_A, INPUT);
     attachPinChangeInterrupt(ENCODER_LEFT_A, Code_left, CHANGE);
     attachPinChangeInterrupt(ENCODER_RIGHT_A, Code_right, CHANGE);
@@ -44,9 +35,5 @@ void setup()
 
 void loop()
 {
- //  Serial.print("left_pluse");
- //  Serial.println(left_pluse);
- //  Serial.print("right_pluse");
- //  Serial.println(right_pluse);
    delay(500);
 }
